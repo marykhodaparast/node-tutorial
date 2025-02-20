836498f4c4c4a3911b7d5a2050337d82bd57b7cb
marykhodaparast <maryam.kh1374@gmail.com>
2025-02-20 17:24:57 +0330

    Modules explined

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-function sayHello(name) {
-    console.log('Hello ' + name)
-}
-//sayHello('Maryam');
-console.log(window);
+// function sayHello(name) {
+//     console.log('Hello ' + name)
+// }
+// //sayHello('Maryam');
+// console.log(window);
+
+//2nd lesson
+// console.log(); //global
+// setTimeout(); //global
+// clearTimeout();
+
+// setInterval();
+// clearInterval();
+
+// var message = '';
+// console.log(global.message);
+
+//3rd lesson -> Modules
+
+// var sayHello = function () {
+    
+// };
+
+// window.sayHello();
+
+
+console.log(module);
